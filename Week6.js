5e5b8b6821f4128c9594e4ab0b557b158ee3fb01
smstalker <sarah.stalker15@outlook.com>
2022-03-12 12:40:15 -0700

    deck built and shuffled

--- Week6.js
+++ Week6.js
@@ -1,6 +1,47 @@
-// Create  an automated version of the classic card game WAR.
-//deck constructor with an empty array
-functionDeck();{
-    this.deck=[];
+const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+const rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
+
+class Deck {
+    constructor() {
+        this.deck = [];
+        
+    }
+
+    buildDeck() {
+        
+        for (let i = 0; i < suits.length; i++) {
+            for (let j = 0; j < rank.length; j++) {
+             this.deck.push({
+                suits: suits[i],
+                rank: rank[j],
+                value: j+1
+             });
+            }
+        }
+        return this.deck; 
+    }
+
+    shuffle() {
+        let count = this.deck.length;
+            while(count) {
+                this.deck.push(this.deck.splice(Math.floor(Math.random() * count), 1)[0]);
+                count -= 1;
+        }
+        return this.deck; 
+    }
+     
 }
+
+
+const fullDeck = new Deck(); 
+
+fullDeck.buildDeck();
+
+//shuffle deck
+fullDeck.shuffle(); 
+
+
+//check to see if all cards are in the deck
+console.log(fullDeck); 
+
 
